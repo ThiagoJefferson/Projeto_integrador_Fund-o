d1fa592c15022ef2eee8e4d5c2d40562598d655d
Your Name <you@example.com>
2018-03-06 19:44:39 -0300

    tela de cadastro

--- cadastro.php
+++ cadastro.php
@@ -12,17 +12,16 @@
 		$banco = 'skate';
 		$conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
 		mysql_select_db($banco) or die(mysql_error());
-	?>
-	<?php
+
 		$nome = $_POST['nome'];
 		$telefone = $_POST['telefone'];
 		$email = $_POST['email'];
 		$senha = $_POST['senha'];
 		$cep = $_POST['cep'];
-		$endereço = $_POST['endereço'];
+		$endereco = $_POST['endereco'];
 		$complemento = $_POST['complemento'];
 
-		$sql = mysql_query("INSERT INTO cliente(nome, telefone, email, senha, cep, endereço, complemento) VALUES ('$nome', '$telefone', '$email', '$senha', '$cep', '$endereço', '$complemento')");
+		$sql = mysql_query("INSERT INTO cliente(nome, telefone, email, senha, cep, endereco, complemento) VALUES ('$nome', '$telefone', '$email', '$senha', '$cep', '$endereco', '$complemento')");
 
 		echo "<center><h1>Seu cadastro foi realizado com sucesso!</h1></center>";
 	?>
